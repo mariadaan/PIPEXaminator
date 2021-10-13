c26e3451560211e0f42b72dc77573975ed3a3327
Maria Daan <mariadaan@gmail.com>
2021-10-13 13:38:19 +0200

    pipex tester

---
+++ all_tests.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+PINK="\033[38;2;255;105;180m"
+GREEN="\033[38;2;57;181;74m"
+RED="\033[38;2;222;56;43m"
+RESET="\033[0m"
+EXIT_CODE_TOTAL
+
+# valid arguments
+printf "$PINK"
+echo "${bold}TEST 1"
+echo "valid arguments${normal}"
+printf "$RESET"
+bash single_test.sh files_in/file_in "cat -e" "grep gewoon" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# valid arguments
+printf "$PINK"
+echo "${bold}TEST 2"
+echo "valid arguments${normal}"
+printf "$RESET"
+bash single_test.sh files_in/file_in "cat -t" "grep FANTASTISCH" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# valid arguments
+printf "$PINK"
+echo "${bold}TEST 3"
+echo "valid arguments${normal}"
+printf "$RESET"
+bash single_test.sh files_in/file_in "ls -a" "grep pipex" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# valid arguments
+printf "$PINK"
+echo "${bold}TEST 4"
+echo "valid arguments${normal}"
+printf "$RESET"
+bash single_test.sh files_in/file_in "ls -l" "wc -l" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# valid arguments
+printf "$PINK"
+echo "${bold}TEST 4"
+echo "valid arguments${normal}"
+printf "$RESET"
+bash single_test.sh files_in/file_in "ls -l" "wc" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# input file does not exist
+printf "$PINK"
+echo "${bold}TEST 6"
+echo "input file does not exist${normal}"
+printf "$RESET"
+bash single_test.sh fiwefcen/fileewe_in "cat -e" "grep gewoon" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# first command does not exist, second command doesnt need input
+printf "$PINK"
+echo "${bold}TEST 7"
+echo "first command does not exist, second command doesnt need input${normal}"
+printf "$RESET"
+bash single_test.sh files_in/file_in "whateverrr" "ls" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# both commands do not exist
+printf "$PINK"
+echo "${bold}TEST 8"
+echo "both commands do not exist${normal}"
+printf "$RESET"
+bash single_test.sh files_in/file_in "whateverrr" "nothing" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# first command does not need input, second command does not exist
+printf "$PINK"
+echo "${bold}TEST 9"
+echo "first command does not need input, second command does not exist${normal}"
+printf "$RESET"
+bash single_test.sh files_in/file_in "ls" "nopeee" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# incorrect number of arguments ( 1 arg )
+printf "$PINK"
+echo "${bold}TEST 10"
+echo "incorrect number of arguments ( 1 arg, input file only )${normal}"
+printf "$RESET"
+touch files_out/res_mine
+bash single_test.sh files_in/file_in
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# incorrect number of arguments ( 2 args, input file and first command only )
+printf "$PINK"
+echo "${bold}TEST 11"
+echo "incorrect number of arguments ( 2 args, input file and first command only )${normal}"
+printf "$RESET"
+touch files_out/res_mine
+bash single_test.sh files_in/file_in cat
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# incorrect number of arguments ( 3 args, input file and two commands )
+printf "$PINK"
+echo "${bold}TEST 12"
+echo "incorrect number of arguments ( 3 args, input file and two commands )${normal}"
+printf "$RESET"
+touch files_out/res_mine
+bash single_test.sh files_in/file_in "cat" "grep gewoon"
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# non-existing flag command 1
+printf "$PINK"
+echo "${bold}TEST 13"
+echo "non-existing flag command 1${normal}"
+printf "$RESET"
+touch files_out/res_mine
+bash single_test.sh files_in/file_in "cat -bestaatniet" "grep FANTASTISCH" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# non-existing flag command 2
+printf "$PINK"
+echo "${bold}TEST 14"
+echo "non-existing flag command 2${normal}"
+printf "$RESET"
+touch files_out/res_mine
+bash single_test.sh files_in/file_in "cat -t" "cat -bestaatniet FANTASTISCH" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+# non-existing flag command 2
+printf "$PINK"
+echo "${bold}TEST 15"
+echo "non-existing flag command 2${normal}"
+printf "$RESET"
+touch files_out/res_mine
+bash single_test.sh files_in/file_in "cat -t" "grep -bestaatniet FANTASTISCH" files_out/res_mine
+EXIT_CODE=$?
+EXIT_CODE_TOTAL=$((EXIT_CODE_TOTAL+EXIT_CODE))
+
+if [ $EXIT_CODE_TOTAL -eq 0 ]; then
+  printf "$GREEN"
+  echo "${bold}========================================= PASS =========================================${normal}"
+  printf "$RESET"
+  echo "Congrats, you passed all tests!"
+else
+  printf "$RED"
+  echo "${bold}========================================= FAIL =========================================${normal}"
+  printf "$RESET"
+  echo "$EXIT_CODE_TOTAL tests have not given the correct output or exitcode. Check terminal output for more
+information on which tests have failed."
+fi
+
+echo -e "\nMake sure to check whether error messages are the same, this tester does not check those.
+Some error messages may look weird because of the shell script information. Run the
+'my command' and the 'real command' of those tests in your terminal to check whether the
+error messages are the same."
+
+echo -e "\n"
+rm -f files_out/res_real
+rm -f files_out/res_mine
---
+++ single_test.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+<<com
+USAGE
+
+create pipex executable first, then run this program.
+
+run command in following format:
+bash single_test.sh file_in cmd1 cmd2 file_out
+
+example:
+bash single_test.sh files_in/file_in "cat -e" "grep gewoon" files_out/res_mine
+com
+
+GREEN="\033[38;2;57;181;74m"
+RED="\033[38;2;222;56;43m"
+RESET="\033[0m"
+BLUE="\033[38;2;34;183;235m"
+RETURN_VALUE=0
+
+RES_REAL="files_out/res_real"
+DIFF_FILE="files_out/diff.txt"
+
+rm -f "files_out/res_mine"
+rm -f $RES_REAL
+rm -f $DIFF_FILE
+
+if [ -z "$4" ]; then
+  unset -v RES_REAL
+fi
+
+ARG2=$2
+echo ""
+echo "my command:   "./pipex $1 \"$2\" \"$3\" $4
+echo "real command: <" $1 $2 "|" $3 ">" $RES_REAL
+echo ""
+
+printf "$BLUE"
+echo "your error message:"
+printf "$RESET"
+./pipex $1 "$2" "$3" $4
+MY_EXITCODE=$?
+echo ""
+
+printf "$BLUE"
+echo "real error message:"
+printf "$RESET"
+< $1 $2 | $3 > $RES_REAL
+REAL_EXITCODE=$?
+
+echo ""
+
+if cmp --silent -- "$4" "$RES_REAL"; then
+  printf "$GREEN"
+  echo "###    SUCCESS: Output Files Are Identical!    ###"
+  printf "$RESET"
+elif [ -z "$4" ]; then
+  printf "$GREEN"
+  echo "###    SUCCESS: Output Files Are Not Made!    ###"
+  printf "$RESET"
+else
+  printf "$RED"
+  echo "###    ERROR:   Output Files Are Different!   ###"
+  RETURN_VALUE=1
+  printf "$RESET"
+fi
+
+if [ $MY_EXITCODE -eq $REAL_EXITCODE ]; then
+  printf "$GREEN"
+  echo "###    SUCCESS: Exit Codes Are Identical!      ###"
+else
+  printf "$RED"
+  RETURN_VALUE=1
+  echo "###    ERROR:   Exit Codes Are Different!     ###"
+fi
+
+echo -n "your exit code: "
+echo $MY_EXITCODE
+echo -n "real exit code: "
+echo $REAL_EXITCODE
+printf "$RESET"
+echo ""
+echo ""
+exit $RETURN_VALUE
